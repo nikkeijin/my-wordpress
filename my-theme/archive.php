544185f2250ae8cc82d4a1b518b8907b1c3e32ab
David <77265282+nikkeijin@users.noreply.github.com>
2023-10-02 14:12:58 +0900

    Update archive.php

--- my-theme/archive.php
+++ my-theme/archive.php
@@ -1,6 +1,6 @@
 <?php 
 
-    // Remember to enable 'Has Archive' option for CPT to use the following method
+    // Remember to change permalink structure to /%postname%/ and remember to enable 'Has Archive' option for CPT to use the following method
     if (is_post_type_archive('news')) locate_template('custom-post-type/archive/news.php', true);
     if (is_post_type_archive('portfolio')) locate_template('custom-post-type/archive/portfolio.php', true);
 
