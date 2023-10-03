30177b712e3f84355bf34929b69a0d5e5838744b
David <77265282+nikkeijin@users.noreply.github.com>
2023-10-03 09:49:10 +0900

    Update portfolio.php

--- my-theme/custom-post-type/single/portfolio.php
+++ my-theme/custom-post-type/single/portfolio.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
 
+    <?php $prev_post = get_previous_post(); if (!empty($prev_post)): ?>
+        <a href="<?php echo get_permalink($prev_post->ID); ?>" class="prev-post"> &laquo; Previous</a>
+    <?php endif; ?>
+
+    <?php $next_post = get_next_post(); if (!empty($next_post)): ?>
+        <a href="<?php echo get_permalink($next_post->ID); ?>" class="next-post"> Next &raquo;</a>
+    <?php endif; ?>
+
 <?php get_footer(); ?>
