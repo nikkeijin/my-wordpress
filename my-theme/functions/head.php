3c9a98fa67120f622e0b5d6e49465b9b8513d2a5
David <77265282+nikkeijin@users.noreply.github.com>
2023-10-31 12:29:14 +0900

    Update head.php

--- my-theme/functions/head.php
+++ my-theme/functions/head.php
@@ -23,13 +23,6 @@ function sample()
 }
 add_action('wp_enqueue_scripts', 'sample');
 
-?>
-
-
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
-
-
-<?php
 
 /* 
 
