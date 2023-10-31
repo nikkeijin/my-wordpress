7813c6cbe460047660051a258bad682f7580441c
David <77265282+nikkeijin@users.noreply.github.com>
2023-10-31 12:29:38 +0900

    Update header.php

--- my-theme/header.php
+++ my-theme/header.php
@@ -16,6 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
     <?php wp_head(); ?>
 
