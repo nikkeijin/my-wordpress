c276dc2a83dcf7d40eeb6db2047dc1c2a86d3a19
David <77265282+nikkeijin@users.noreply.github.com>
2023-11-06 10:05:52 +0900

    Update front-page.php

--- my-theme/pages/front-page.php
+++ my-theme/pages/front-page.php
@@ -1,1 +1,24 @@
 <h1>Front Page</h1>
+
+<?php
+
+  // Args for querying posts of the 'news' post type
+  $args = array(
+      'post_type'      => 'news',
+      'posts_per_page' => 10,
+  );
+
+  // Create a new query object
+  $query = new WP_Query($args);
+
+?>
+
+<!-- The Loop -->
+<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+    <?php the_title(); ?>
+<?php endwhile; endif; ?>
+
+<?php
+  // Reset post data when you're done with the custom query
+  wp_reset_postdata();
+?>
