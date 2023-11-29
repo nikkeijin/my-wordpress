b5d3e3b33e448b4d10eab3bd2404c5186083f49c
David <77265282+nikkeijin@users.noreply.github.com>
2023-11-29 16:37:15 +0900

    Update author.php

--- my-theme/author.php
+++ my-theme/author.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
+    <!--
+        ################################################## 
+
+        To use this template, you gotta disable the following code from your functions/theme.php: 
+        add_action('template_redirect', 'redirect_author_archive_to_home');
+
+    -->
+
     <!-- SETTINGS -->
     <?php $user_data = get_userdata($author); ?>
     <?php $author_avatar = get_avatar($user_data->ID); ?>
